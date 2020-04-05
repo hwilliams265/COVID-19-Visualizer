dfbb5360068db55eb1434c5b4197d61764a3f80d
Harry Williams <hwill@bu.edu>
2020-04-05 16:54:43 -0400

    added basic map functionality

--- app/src/main/java/com/example/covid_19visualizer/MainActivity.java
+++ app/src/main/java/com/example/covid_19visualizer/MainActivity.java
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view,"This could be a link to a coronavirus symptom checker or" +
+                        " something, idk", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
